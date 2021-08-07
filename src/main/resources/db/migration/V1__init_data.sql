3311526ae4e9f72380420ed0c1f96373ddaad4ed
Расулов Теймур Эминович <terasulov@vtb.ru>
2021-08-08 01:04:48 +0300

    db fix

--- src/main/resources/db/migration/V1__init_data.sql
+++ src/main/resources/db/migration/V1__init_data.sql
@@ -9,12 +9,7 @@ insert into cars (model, power, price, brand_id)
 values ('m3', 500, 10000000, 1),
        ('m2 competition', 1000, 100000000, 1);
 
--- CREATE TABLE users (id bigserial primary key, login varchar(255), password varchar(255), wallet integer, firstname varchar(255), lastname varchar(255));
--- INSERT INTO users (login, password, wallet, firstname, lastname)
--- values ('login', 'password', 1000000, 'мистер', 'тест'),
---         ('admin', 'password', 10000000, 'мистер', 'суперАдмин');
-
-CREATE TABLE car_options (id bigserial primary keep, car_id bigint references cars (id), option_name varchar (255));
+CREATE TABLE car_options (id bigserial primary key, car_id bigint references cars (id), option_name varchar (255));
 insert into car_options (car_id, option_name)
 values (1, 'Черная крыша'),
        (1, 'Алькантара'),
@@ -24,4 +19,4 @@ values (1, 'Черная крыша'),
        (2, 'Диски m competition'),
        (2, 'Мотор 800 л.с.'),
        (2, 'Расход масла 100 л на 100 км'),
-       (2, 'Парковочный ассистент'),
+       (2, 'Парковочный ассистент');
